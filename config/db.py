dda9a87ef69d7e2ec1ea4e365f334a7c78b467f9
Jeevanandhamskein <jeevanandham.m@skeintech.com>
2022-09-22 18:16:09 +0530

    JJJ

--- config/db.py
+++ config/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine,MetaData
 
-engine=create_engine("mysql+mysqldb://root:root@localhost:3306/thirddb")
+engine=create_engine("mysql+mysqldb://root:root-1234ROOT@localhost:3306/thirddb")
 #engine=create_engine("mysql+mysqldb(pip:mysqlclient)://uname:password@localhost:3306/dbname")
 
 conn=engine.connect()
